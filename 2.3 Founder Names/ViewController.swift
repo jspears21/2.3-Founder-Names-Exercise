1afa8c1220d108412923a1d981a464986997de47
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-08-29 16:13:37 -0700

    quizzzz

--- 2.3 Founder Names/ViewController.swift
+++ 2.3 Founder Names/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Jared Spears on 8/29/19.
 //  Copyright © 2019 Jared Spears. All rights reserved.
 //
-// The advantages of coding this way is that arrays allow programmers to type less and reference variable other than hand typing every time. They also allow ease of updating a list if the names were to change. Then all you need to do is change the names in the array variable and not everywhere else. 
+//// The advantages of coding this way is that arrays allow programmers to type less and reference variable other than hand typing every time. They also allow ease of updating a list if the names were to change. Then all you need to do is change the names in the array variable and not everywhere else the names occur.
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,12 +14,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var founder2: UILabel!
     
+      var founders = ["Sergey Brin","Larry Page","Jennifer Hyman","Jenny Fleiss"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    var founders = ["Sergey Brin","Larry Page","Jennifer Hyman","Jenny Fleiss"]
+  
     
     @IBAction func bGoogle(_ sender: UIButton) {
         founder1.text = founders[0]
